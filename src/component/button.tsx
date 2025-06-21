44b1db866a14be4c634580b6f5be8ba7b3a61365
Johnny Gérard <john@jgerard.dev>
2025-06-21 13:10:15 +0200

    feat: allow Button component to use an anchor element

--- src/component/button.tsx
+++ src/component/button.tsx
@@ -1,17 +1,30 @@
 import { clsx } from "clsx";
+import Link, { LinkProps } from "next/link";
 import {
+  AnchorHTMLAttributes,
   ButtonHTMLAttributes,
   DetailedHTMLProps,
   memo,
   ReactNode,
 } from "react";
 
+type AnchorProps = DetailedHTMLProps<
+  AnchorHTMLAttributes<HTMLAnchorElement>,
+  HTMLAnchorElement
+>;
+
 type ButtonProps = DetailedHTMLProps<
   ButtonHTMLAttributes<HTMLButtonElement>,
   HTMLButtonElement
 >;
 
-type Props = ButtonProps & {
+type Props = (
+  | ({ asLink: true; asNextLink?: false } & AnchorProps)
+  | ({ asLink?: false; asNextLink: true } & LinkProps)
+  | ({ asLink?: false; asNextLink?: false } & ButtonProps)
+) & {
+  children: ReactNode;
+  className?: string;
   icon?: ReactNode;
   iconLeft?: ReactNode;
   iconRight?: ReactNode;
@@ -29,9 +42,10 @@ type Props = ButtonProps & {
 
 export const Button = memo(
   ({
+    asLink,
+    asNextLink,
     children,
     className,
-    disabled,
     icon,
     iconLeft,
     iconRight,
@@ -44,68 +58,76 @@ export const Button = memo(
       size === "2xl" ? "size-6 p-0.5" : "size-5 p-[0.10419rem]",
     );
 
-    return (
-      <button
-        disabled={disabled}
-        className={clsx(
-          // Base style
-          [
-            "flex items-center justify-center rounded font-medium",
-            "transition-[background-color,color,outline-color,box-shadow]",
-          ],
-          // Disabled and type-specific style
-          disabled
-            ? [
-                "cursor-not-allowed text-neutral-400",
-                isLinkVariant || "bg-neutral-100",
-              ]
-            : {
-                primary: "bg-indigo-700 text-white hocus:bg-indigo-800",
-                secondary: [
-                  "bg-white text-neutral-900 inset-ring-[0.5px] inset-ring-neutral-200",
-                  "hocus:bg-neutral-50 hocus:text-neutral-950 hocus:inset-ring",
-                ],
-                tertiary: "text-indigo-700 hocus:bg-neutral-50",
-                linkColor: "text-indigo-700 hocus:text-indigo-800",
-                linkGray: "text-neutral-600 hocus:text-neutral-900",
-                destructive: "bg-red-600 text-white hocus:bg-red-700",
-              }[type],
-          // Focus ring
-          type === "destructive"
-            ? "focus-visible:[box-shadow:0_0_0_1px_#D92D20,0_0_0_4px_rgba(217,45,32,0.12)]"
-            : "outline-4 focus-visible:outline-[rgba(68,76,231,0.12)]",
-          // Shadow
-          ["primary", "secondary"].includes(type) &&
-            "not-disabled:not-focus-visible:shadow",
-          // Padding
-          isLinkVariant ||
-            {
-              md: iconOnly ? "p-2.5" : "px-3.5 py-2.5",
-              lg: iconOnly ? "p-3" : "px-4 py-2.5",
-              xl: iconOnly ? "p-3.5" : "px-5 py-3",
-              "2xl": iconOnly ? "p-4" : "px-6 py-4",
-            }[size],
-          // Gap and font size
-          iconOnly ||
-            {
-              md: "gap-1 text-sm",
-              lg: "gap-1.5 text-base",
-              xl: "gap-1.5 text-base",
-              "2xl": "gap-2.5 text-lg",
-            }[size],
-          className,
-        )}
-        {...props}
-      >
-        {icon ? (
-          <div className={iconStyle}>{icon}</div>
-        ) : (
-          <>
-            {iconLeft && <div className={iconStyle}>{iconLeft}</div>}
-            <span className="px-0.5">{children}</span>
-            {iconRight && <div className={iconStyle}>{iconRight}</div>}
-          </>
-        )}
+    const buttonStyle = clsx(
+      // Base style
+      [
+        "flex items-center justify-center rounded font-medium",
+        "transition-[background-color,color,outline-color,box-shadow]",
+      ],
+      // Disabled and type-specific style
+      "disabled" in props && props.disabled
+        ? [
+            "cursor-not-allowed text-neutral-400",
+            isLinkVariant || "bg-neutral-100",
+          ]
+        : {
+            primary: "bg-indigo-700 text-white hocus:bg-indigo-800",
+            secondary: [
+              "bg-white text-neutral-900 inset-ring-[0.5px] inset-ring-neutral-200",
+              "hocus:bg-neutral-50 hocus:text-neutral-950 hocus:inset-ring",
+            ],
+            tertiary: "text-indigo-700 hocus:bg-neutral-50",
+            linkColor: "text-indigo-700 hocus:text-indigo-800",
+            linkGray: "text-neutral-600 hocus:text-neutral-900",
+            destructive: "bg-red-600 text-white hocus:bg-red-700",
+          }[type],
+      // Focus ring
+      type === "destructive"
+        ? "focus-visible:[box-shadow:0_0_0_1px_#D92D20,0_0_0_4px_rgba(217,45,32,0.12)]"
+        : "outline-4 focus-visible:outline-[rgba(68,76,231,0.12)]",
+      // Shadow
+      ["primary", "secondary"].includes(type) &&
+        "not-disabled:not-focus-visible:shadow",
+      // Padding
+      isLinkVariant ||
+        {
+          md: iconOnly ? "p-2.5" : "px-3.5 py-2.5",
+          lg: iconOnly ? "p-3" : "px-4 py-2.5",
+          xl: iconOnly ? "p-3.5" : "px-5 py-3",
+          "2xl": iconOnly ? "p-4" : "px-6 py-4",
+        }[size],
+      // Gap and font size
+      iconOnly ||
+        {
+          md: "gap-1 text-sm",
+          lg: "gap-1.5 text-base",
+          xl: "gap-1.5 text-base",
+          "2xl": "gap-2.5 text-lg",
+        }[size],
+      className,
+    );
+
+    const content = icon ? (
+      <div className={iconStyle}>{icon}</div>
+    ) : (
+      <>
+        {iconLeft && <div className={iconStyle}>{iconLeft}</div>}
+        <span className="px-0.5">{children}</span>
+        {iconRight && <div className={iconStyle}>{iconRight}</div>}
+      </>
+    );
+
+    return asNextLink ? (
+      <Link className={buttonStyle} {...(props as LinkProps)}>
+        {content}
+      </Link>
+    ) : asLink ? (
+      <a className={buttonStyle} {...(props as AnchorProps)}>
+        {content}
+      </a>
+    ) : (
+      <button className={buttonStyle} {...(props as ButtonProps)}>
+        {content}
       </button>
     );
   },
