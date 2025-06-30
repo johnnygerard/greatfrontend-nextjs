3caec9529e54ed289cf17b7645617f38b5b3ac05
Johnny Gérard <john@jgerard.dev>
2025-06-30 12:14:16 +0200

    feat: implement responsive Navbar with mobile drawer

--- src/component/navbar.tsx
+++ src/component/navbar.tsx
@@ -1,7 +1,11 @@
+"use client";
 import { Button } from "@/component/button";
 import { LogoAbstractly } from "@/component/logo-abstractly";
+import { IconClose } from "@/component/svg/icon-close";
+import { IconHamburgerMenu } from "@/component/svg/icon-hamburger-menu";
 import { clsx } from "clsx";
-import { memo } from "react";
+import Link from "next/link";
+import { memo, useState } from "react";
 
 const links: Array<{
   text: string;
@@ -19,40 +23,126 @@ type Props = {
 };
 
 export const Navbar = memo(({ className }: Props) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
-    <header className={clsx("px-28 pt-4 max-xl:hidden", className)}>
-      <div className="flex items-center gap-24 py-3">
-        <LogoAbstractly />
-        <nav aria-label="Main" className="flex-1">
-          <ul className="flex gap-8">
-            {links.map(({ text, path }) => (
-              <li key={path}>
-                <Button
-                  asNextLink
-                  href={path}
-                  variant={{ size: "lg", type: "linkGray" }}
-                >
-                  {text}
-                </Button>
-              </li>
-            ))}
-          </ul>
-        </nav>
-        <div className="flex gap-4">
+    <header>
+      {/* Desktop */}
+      <div className={clsx("px-28 pt-4 max-xl:hidden", className)}>
+        <div className="flex items-center gap-24 py-3">
+          <LogoAbstractly />
+          <nav aria-label="Main" className="flex-1">
+            <ul className="flex gap-8">
+              {links.map(({ text, path }) => (
+                <li key={path}>
+                  <Button
+                    asNextLink
+                    href={path}
+                    variant={{ size: "lg", type: "linkGray" }}
+                  >
+                    {text}
+                  </Button>
+                </li>
+              ))}
+            </ul>
+          </nav>
+          <div className="flex gap-4">
+            <Button
+              asNextLink
+              href="/learn-more"
+              variant={{ size: "lg", type: "secondary" }}
+            >
+              Learn more
+            </Button>
+            <Button
+              asNextLink
+              href="/pricing"
+              variant={{ size: "lg", type: "primary" }}
+            >
+              See pricing
+            </Button>
+          </div>
+        </div>
+      </div>
+
+      {/* Mobile and tablet */}
+      <div className={clsx("xl:hidden", className)}>
+        {/* Navigation bar */}
+        <div className="flex h-17 items-center justify-between px-4 pt-4 sm:px-8">
+          <LogoAbstractly />
           <Button
-            asNextLink
-            href="/learn-more"
-            variant={{ size: "lg", type: "secondary" }}
+            iconOnly
+            variant={{ size: "sm", type: "linkGray" }}
+            onClick={() => {
+              setIsMenuOpen(true);
+            }}
           >
-            Learn more
+            <IconHamburgerMenu />
           </Button>
-          <Button
-            asNextLink
-            href="/pricing"
-            variant={{ size: "lg", type: "primary" }}
-          >
-            See pricing
-          </Button>
+        </div>
+
+        {/* Backdrop */}
+        <div
+          aria-hidden
+          className={clsx(
+            "fixed inset-0 bg-neutral-950 transition-opacity duration-300 ease-in-out",
+            isMenuOpen ? "opacity-70" : "pointer-events-none opacity-0",
+          )}
+          onClick={() => setIsMenuOpen(false)}
+        />
+
+        {/* Navigation drawer */}
+        <div
+          className={clsx(
+            "flex flex-col gap-6",
+            "fixed top-0 bottom-0 left-0 w-90 max-w-screen bg-white px-4 pt-8 pb-4",
+            "transition-transform duration-300 ease-in-out",
+            isMenuOpen ? "translate-x-0" : "-translate-x-full",
+          )}
+        >
+          <div className="flex items-center justify-between">
+            <LogoAbstractly />
+            <Button
+              iconOnly
+              variant={{ size: "sm", type: "linkGray" }}
+              onClick={() => {
+                setIsMenuOpen(false);
+              }}
+            >
+              <IconClose />
+            </Button>
+          </div>
+          <nav aria-label="Main" className="flex-1">
+            <ul className="space-y-2">
+              {links.map(({ text, path }) => (
+                <li key={path}>
+                  <Link
+                    className="block rounded px-3 py-2 text-sm text-neutral-900 hocus:bg-neutral-100"
+                    href={path}
+                  >
+                    {text}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
+          <div>
+            <Button
+              asNextLink
+              href="/learn-more"
+              variant={{ size: "lg", type: "secondary" }}
+            >
+              Learn more
+            </Button>
+            <Button
+              className="mt-4"
+              asNextLink
+              href="/pricing"
+              variant={{ size: "lg", type: "primary" }}
+            >
+              Try it out
+            </Button>
+          </div>
         </div>
       </div>
     </header>
