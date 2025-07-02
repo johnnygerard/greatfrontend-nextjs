e5fc5e81fc32595dd459b4858010b1584059fc82
Johnny Gérard <john@jgerard.dev>
2025-07-02 19:56:55 +0200

    feat: implement TextInput component solution

--- src/app/solution/text-input-component/page.tsx
+++ src/app/solution/text-input-component/page.tsx
@@ -1,4 +1,5 @@
 import { TextInput } from "@/component/text-input";
+import { RiMailLine, RiQuestionLine } from "@remixicon/react";
 import { Metadata } from "next";
 import { memo } from "react";
 
@@ -8,8 +9,42 @@ export const metadata: Metadata = {
 
 const Page = () => {
   return (
-    <main className="flex min-h-screen justify-center bg-white py-28">
-      <TextInput />
+    <main className="flex min-h-screen justify-center bg-white px-[1.09375rem] py-28">
+      <div className="flex w-85 flex-col gap-12">
+        <TextInput
+          type="email"
+          label="Email"
+          placeholder="name@email.com"
+          iconRight={<RiQuestionLine />}
+          hint="This is a hint text."
+        />
+        <TextInput
+          type="email"
+          label="Email"
+          placeholder="name@email.com"
+          iconLeft={<RiMailLine />}
+          iconRight={<RiQuestionLine />}
+          hint="This is a hint text."
+        />
+        <TextInput
+          type="email"
+          disabled
+          label="Email"
+          placeholder="name@email.com"
+          iconRight={<RiQuestionLine />}
+          hint="This is a hint text."
+        />
+        <TextInput
+          type="email"
+          label="Email"
+          placeholder="name@email.com"
+          defaultValue="name@email.com"
+          iconRight={<RiQuestionLine />}
+          iconRightShowsError
+          error="This is an error message."
+          hint="This is a hint text."
+        />
+      </div>
     </main>
   );
 };
