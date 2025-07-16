287dc5d8bb5146e2c42c5191732a387d04e28e28
Johnny Gérard <john@jgerard.dev>
2025-07-16 23:23:15 +0200

    feat: style 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,6 @@
+import { Button } from "@/component/button";
+import { RiArrowLeftLongLine } from "@remixicon/react";
 import { Metadata } from "next";
-import Link from "next/link";
 import { memo } from "react";
 
 export const metadata: Metadata = {
@@ -8,13 +9,31 @@ export const metadata: Metadata = {
 
 const NotFound = () => {
   return (
-    <div className="grid min-h-screen place-items-center">
-      <div className="text-center">
-        <h1>404 Not Found</h1>
-        <p>Sorry, the page you are looking for does not exist.</p>
-        <Link href="/">Back to Home</Link>
+    <main className="grid min-h-screen place-items-center px-4">
+      <div className="flex max-w-md flex-col items-center text-center">
+        <hgroup>
+          <p className="text-6xl font-bold text-gray-900">404</p>
+          <h1 className="mt-6 text-2xl font-semibold text-gray-800">
+            Page Not Found
+          </h1>
+          <p className="mt-2 text-gray-600">
+            Sorry, the page you are looking for does not exist.
+          </p>
+        </hgroup>
+        <Button
+          asNextLink
+          className="mt-6"
+          href="/"
+          iconLeft={<RiArrowLeftLongLine />}
+          variant={{
+            type: "tertiary",
+            size: "md",
+          }}
+        >
+          Back to Home
+        </Button>
       </div>
-    </div>
+    </main>
   );
 };
 
