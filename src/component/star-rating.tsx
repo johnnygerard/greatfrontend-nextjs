03be1bf956942f142e58e76052b2af66c599add5
Johnny Gérard <john@jgerard.dev>
2025-07-18 19:57:59 +0200

    feat: add StarRating component

---
+++ src/component/star-rating.tsx
@@ -0,0 +1,38 @@
+import { IconStar } from "@/component/svg/icon-star";
+import { clsx } from "clsx";
+import { memo } from "react";
+
+type Props = {
+  className?: string;
+  rating?: number;
+};
+
+const STAR_COUNT = 5;
+
+export const StarRating = memo(({ className, rating = 0 }: Props) => {
+  if (rating < 0 || rating > STAR_COUNT) {
+    console.error(`RangeError: Rating must be between 0 and ${STAR_COUNT}`, {
+      rating,
+    });
+    rating = 0;
+  }
+
+  const label = `Rating: ${rating.toFixed(1)} out of ${STAR_COUNT}`;
+
+  return (
+    <>
+      <span className="sr-only">{label}</span>
+      <div aria-hidden className={clsx("flex gap-1", className)} title={label}>
+        {Array.from({ length: STAR_COUNT }, (_, index) => (
+          <IconStar
+            key={index}
+            className="size-5"
+            rating={Math.max(0, Math.min(1, rating - index))}
+          />
+        ))}
+      </div>
+    </>
+  );
+});
+
+StarRating.displayName = "StarRating";
